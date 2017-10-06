d5c24e8fd9e2c151e388eef29b3055a398d85cac
Brian J Brennan <brianloveswords@gmail.com>
2017-10-06 00:47:15 -0400

    [feat] Implement isArray default validator
    This change also implements the necessary functionality for iterating
    over and applying validators.

--- src/validator.ts
+++ src/validator.ts
@@ -1,13 +1,23 @@
 interface RuleSet<K> {
-    [key: string]: Array<SimpleValidator<K> | ValidatorFunction>;
+    [key: string]: Array<Validator<K>>;
 }
+
+type Validator<K> = SimpleValidator<K> | ValidatorFunction;
 
 type SimpleValidator<ValidatorKey> = [ValidatorKey] | [ValidatorKey, any[]];
 
+type Maybe<T> = undefined | T;
+
 type ValidatorFunction = (
     key: string,
+    value: any,
+    object: any,
     ...args: any[]
 ) => undefined | ValidationError;
+
+interface DefaultValidators {
+    [key: string]: ValidatorFunction;
+}
 
 interface ValidationError {
     key: string;
@@ -17,14 +27,53 @@ interface ValidationError {
 
 type ValidationResult = [boolean, ValidationError[]];
 
-export class ConfigValidator<K> {
-    private rules: RuleSet<K>;
+export class ConfigValidator<ValidatorKey extends keyof DefaultValidators> {
+    private rules: RuleSet<ValidatorKey>;
 
-    constructor(rules: RuleSet<K>) {
+    constructor(rules: RuleSet<ValidatorKey>) {
         this.rules = rules;
     }
 
-    public validate(input: object): ValidationResult {
-        return [true, []];
+    public validate(input: any): ValidationResult {
+        const errors: ValidationError[] = [];
+        for (const [key, validators] of Object.entries(this.rules)) {
+            for (const validator of validators) {
+                const [fn, args] = this.unpackRule(validator);
+                const possibleError = fn(key, input[key], input, ...args);
+                if (possibleError) {
+                    errors.push(possibleError);
+                }
+            }
+        }
+        return [errors.length === 0, errors];
+    }
+
+    private unpackRule(
+        validator: Validator<ValidatorKey>,
+    ): [ValidatorFunction, any[]] {
+        let validationFn;
+        let args: any[] = [];
+        if (typeof validator === "function") {
+            validationFn = validator;
+        } else {
+            const name = validator[0];
+            validationFn = DEFAULT_VALIDATORS[name];
+            if (validator.length === 2) {
+                args = validator[1] as any[];
+            }
+        }
+        return [validationFn, args];
     }
 }
+
+const DEFAULT_VALIDATORS: DefaultValidators = {
+    isArray: (key, value): Maybe<ValidationError> => {
+        return !Array.isArray(value)
+            ? {
+                key,
+                message: `${key} must be an Array`,
+                validator: "isArray",
+            }
+            : undefined;
+    },
+};
--- test/validator.test.ts
+++ test/validator.test.ts
@@ -12,6 +12,17 @@ describe("ConfigValidator", () => {
                 expect(passed).toBe(true);
                 expect(errors).toHaveLength(0);
             });
+            it("should not pass when it's not an array", () => {
+                const [passed, errors] = new ConfigValidator({
+                    firstKey: [["isArray"]],
+                }).validate({
+                    firstKey: 1,
+                });
+                expect(passed).toBe(false);
+                expect(errors).toHaveLength(1);
+                expect(errors[0].key).toBe("firstKey");
+                expect(errors[0].validator).toBe("isArray");
+            });
         });
     });
 });
